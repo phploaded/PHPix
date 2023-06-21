cccf9ad93ae3df2ab0c3ef2a02c2a51fe7fedbbe
phploaded <satish@phploaded.com>
2023-06-21 06:54:17 +0530

    Update phpix-info.php

--- phpix-info.php
+++ phpix-info.php
@@ -1,8 +1,8 @@
 <?php 
 
-$software_version = '1.67';
+$software_version = '1.68';
 $software_date = '1603041199';
-$software_updated = '1603646191';
+$software_updated = '1687310367';
 $software_jsonURL = 'https://raw.githubusercontent.com/phploaded/phpix-packages/main/phpix-updates/updates.json';
 $software_zipURL = 'https://raw.githubusercontent.com/phploaded/phpix-packages/main/phpix-updates/';
 
